5abd84503e6982424ba06305a61f690b4749b69a
CaptainVikkus <jaketreleaven@gmail.com>
2020-10-07 16:18:37 -0400

    Project Commit
    Initial Commit of Unity Project files and basic assets

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        move *= Time.deltaTime * speed;
+
+        transform.Translate(move);
+    }
+}
