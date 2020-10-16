22df2a572de7b6f91cf98f34018495efc6813e0d
FlavioJorge AraujoSilva <silvaflavio267@gmail.com>
2020-10-15 21:25:53 -0400

    Added more function to the battle system
    Working Moves, Basic Animations, Dialog to show when pokemon did critcial hits and super or not effectieve moves

--- Assets/Scripts/BattleSystem/HPBar.cs
+++ Assets/Scripts/BattleSystem/HPBar.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class HPBar : MonoBehaviour
 {
     [SerializeField] GameObject heath;
-    // Start is called before the first frame update
-    void Start()
-    {
-        heath.transform.localScale = new Vector3(0.5f, 1.0f);
-        
-    }
 
     public void SetUpHP (float hpNormalized)
     {
         heath.transform.localScale = new Vector3(hpNormalized, 1.0f);
+    }
+
+    public IEnumerator SetHpSmooth(float newHpNormalized)
+    {
+        float curHPNormalized = heath.transform.localScale.x;
+        float changeAmt = curHPNormalized - newHpNormalized;
+
+        while (curHPNormalized - newHpNormalized > Mathf.Epsilon)
+        {
+            curHPNormalized -= changeAmt * Time.deltaTime;
+            SetUpHP(curHPNormalized);
+            yield return null;
+        }
+
+        SetUpHP(newHpNormalized);
     }
 }
