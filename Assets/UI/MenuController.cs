4e6268d3f50f7998e6a123c4553dade812c38fb0
CaptainVikkus <jaketreleaven@gmail.com>
2020-12-10 12:52:57 -0500

    Menus!
    Added a menu icon with animation
    Added menu with resume/save/quit
    functionality for save not implemented

---
+++ Assets/UI/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public Animator menuButton;
+    public GameObject mainMenu;
+
+    public void ToggleMenu()
+    {
+        menuButton.SetBool("Open", !menuButton.GetBool("Open"));
+        mainMenu.SetActive(!mainMenu.activeSelf);
+    }
+}
